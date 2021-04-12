b2ce56de386fa18d1b65c0f37aac16e4efebfcd0
Danielle <danielle_meerburg@live.nl>
2021-04-12 12:40:12 +0200

    imports_e.d

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeXp } from './components/IncomeXp'
+import { TaskList } from './components/TaskList'
+import { AddTask } from './components/AddTask'
 
+import { GlobalProvider } from './context/GlobalState'
 import './App.css'
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
      <Header />
-    </div>
+     <div className="container">
+       <Balance />
+       <IncomeXp />
+       <TaskList />
+       <AddTask />
+     </div>
+    </GlobalProvider>
   );
 }
 
